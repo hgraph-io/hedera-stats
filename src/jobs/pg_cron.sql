16617d81c942bc829704ea3fefec311cb4d0b646
Tyler McDonald <tyler@hgraph.io>
2025-03-18 21:12:47 -0600

    add additional jobs (#15)

--- src/jobs/pg_cron.sql
+++ src/jobs/pg_cron.sql
@@ -1,4 +1,61 @@
--- hourly job
+--------------------------------
+-- every 5 minutes
+--------------------------------
+select cron.schedule_in_database(
+    'refresh materialized view concurrently transactions_last_24hrs',
+    '*/5 * * * *',
+    'refresh materialized view concurrently transactions_last_24hrs;',
+    '<database_name>',
+    '<database_user>'
+);
+
+select cron.schedule_in_database(
+    'refresh materialized view concurrently total_accounts',
+    '*/5 * * * *',
+    'refresh materialized view concurrently total_accounts;',
+    '<database_name>',
+    '<database_user>'
+);
+
+select cron.schedule_in_database(
+    'refresh materialized view concurrently contract_transactions_last_24hrs',
+    '1-59/5 * * * *',
+    'refresh materialized view concurrently contract_transactions_last_24hrs;',
+    '<database_name>',
+    '<database_user>'
+);
+--------------------------------
+-- hourly
+--------------------------------
+select cron.schedule_in_database(
+    'call ecosystem.load_transaction_metrics()',
+    -- hourly at minute 1
+    '1 * * * *',
+    'call ecosystem.load_transaction_metrics();',
+    '<database_name>',
+    '<database_user>'
+);
+
+
+select cron.schedule_in_database(
+    'call ecosystem.load_fees_by_transaction_type()',
+    -- hourly at minute 1
+    '1 * * * *',
+    'call ecosystem.load_fees_by_transaction_type();',
+    '<database_name>',
+    '<database_user>'
+);
+
+select cron.schedule_in_database(
+    'call ecosystem.load_network_deposits()',
+    -- every hour at minute 1
+    '1 * * * *',
+    'call ecosystem.load_network_deposits();',
+    '<database_name>',
+    '<database_user>'
+);
+
+
 select cron.schedule_in_database(
     'call ecosystem.load_hourly_metrics()',
     -- hourly at minute 3
@@ -7,8 +64,9 @@ select cron.schedule_in_database(
     '<database_name>',
     '<database_user>'
 );
-
--- daily job
+--------------------------------
+-- daily
+--------------------------------
 select cron.schedule_in_database(
     'call ecosystem.load_metrics()',
     -- daily at midnight
@@ -18,27 +76,24 @@ select cron.schedule_in_database(
     '<database_user>'
 );
 
--- daily job
 select cron.schedule_in_database(
     'call ecosystem.load_network_tvl()',
     -- daily after midnight UTC
-    '2 * * * *',
+    '2 0 * * *',
     'call ecosystem.load_network_tvl();',
     '<database_name>',
     '<database_user>'
 );
 
--- daily job
 select cron.schedule_in_database(
     'call ecosystem.load_stablecoin_marketcap()',
     -- daily after midnight UTC
-    '3 * * * *',
+    '3 0 * * *',
     'call ecosystem.load_stablecoin_marketcap()',
     '<database_name>',
     '<database_user>'
 );
 
--- daily job
 select cron.schedule_in_database(
     'refresh materialized concurrently view ecosystem.hashgraph_dashboard',
     -- daily after midnight UTC
@@ -47,3 +102,16 @@ select cron.schedule_in_database(
     '<database_name>',
     '<database_user>'
 );
+
+--------------------------------
+-- weekly
+--------------------------------
+select cron.schedule_in_database(
+    'refresh materialized view concurrently ecosystem.active_nft_account_cohorts',
+    -- weekly on Sunday at midnight
+    '0 0 * * 0',
+    'refresh materialized view concurrently ecosystem.active_nft_account_cohorts;',
+    '<database_name>',
+    '<database_user>'
+);
+
