f22d0c8e38f935a50a87221642feb4b1763ba64d
Tyler McDonald <tyler@hgraph.io>
2025-05-19 11:40:22 -0600

    add daily metrics for a few metrics

--- src/jobs/load_daily_metrics.sql
+++ src/jobs/load_daily_metrics.sql
@@ -1,29 +1,13 @@
--- hourly job
-create or replace procedure ecosystem.load_hourly_metrics()
+-- daily job
+create or replace procedure ecosystem.load_daily_metrics()
 language plpgsql
 as $$
 
 declare
 
     metrics text[] := array [
-        'accounts_associating_nfts',
-        'accounts_receiving_nfts',
-        'accounts_sending_nfts',
-        'accounts_minting_nfts',
-        'accounts_creating_nft_collections',
-        'active_nft_accounts',
-        'active_nft_builder_accounts',
-        'nft_collections_created',
-        'nfts_minted',
-        'nfts_transferred',
-        'nft_sales_volume',
-        'active_developer_accounts',
-        'active_retail_accounts',
-        'active_smart_contracts',
-        'active_accounts',
-        'network_fee',
-        'account_growth',
-        'network_tps'
+      'network_fee',
+      'network_tps'
     ];
     metric_name text;
 
@@ -36,10 +20,10 @@ begin
     set time zone 'utc';
     total_time := clock_timestamp();
 
-    -- Truncate current time to hour so we don't include a partial hour
-    end_timestamp_bigint := date_trunc('hour', now())::timestamp9::bigint;
+    -- Truncate current time to day so we don't include a partial day
+    end_timestamp_bigint := date_trunc('day', now())::timestamp9::bigint;
 
-    raise notice 'loading hourly metrics up to % (utc)', (end_timestamp_bigint)::timestamp9;
+    raise notice 'loading daily metrics up to % (utc)', (end_timestamp_bigint)::timestamp9;
 
     foreach metric_name in array metrics loop
         metric_loop_time := clock_timestamp();
@@ -47,7 +31,7 @@ begin
         select coalesce(max(upper(timestamp_range)), 0)
           into last_upper_bound
           from ecosystem.metric
-         where period = 'hour'
+         where period = 'day'
            and name = metric_name;
 
         raise notice 'metric: %, last_upper_bound: % => % (utc)',
@@ -56,15 +40,15 @@ begin
                      (last_upper_bound)::timestamp9;
 
         if last_upper_bound >= end_timestamp_bigint then
-            raise notice 'no new full hours to insert for metric %', metric_name;
+            raise notice 'no new full days to insert for metric %', metric_name;
         else
             execute format($sql$
                 insert into ecosystem.metric (name, period, timestamp_range, total)
                 select %L as name,
-                       'hour' as period,
+                       'day' as period,
                        int8range,
                        total
-                  from ecosystem.%I('hour', %s, %s)
+                  from ecosystem.%I('day', %s, %s)
                   where upper(int8range) is not null
                 on conflict (name, period, timestamp_range)
                 do update
