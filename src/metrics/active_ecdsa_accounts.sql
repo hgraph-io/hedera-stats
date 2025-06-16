53b0f2a3b34d1c1d182c59fd051929369a65a7cb
itsbrandond <brandon@hgraph.com>
2025-06-16 14:42:09 -0400

    re-factored stats

--- src/metrics/active_ecdsa_accounts.sql
+++ src/metrics/active_ecdsa_accounts.sql
@@ -1,35 +1,25 @@
-create or replace function ecosystem.active_ecdsa_accounts(
-    period text,
-    start_timestamp bigint default 0,
-    end_timestamp bigint default CURRENT_TIMESTAMP::timestamp9::bigint
-)
-returns setof ecosystem . metric_total
-as $$
-with raw_active_accounts as (
-  select * from ecosystem.active_developer_accounts(period, start_timestamp, end_timestamp)
-  union all
-  select * from ecosystem.active_retail_accounts(period, start_timestamp, end_timestamp)
-),
-active_ecdsa_accounts as (
-  select d.*
-  from raw_active_accounts d
-  join public.entity e
-    on d.account_id = e.id
-   and encode(substring(e.key from 1 for 1), 'hex') = '12' 
-),
-merged_data as (
-  select
-    lower(int8range) as period_start_timestamp,
-    sum(total) as total
-  from active_ecdsa_accounts
-  group by 1
-  order by 1 desc
-)
-select
-  int8range(
-    period_start_timestamp::timestamp9::bigint,
-    coalesce((lead(period_start_timestamp) over (order by period_start_timestamp rows between current row and 1 following))::timestamp9::bigint, end_timestamp)
-  ),
-  total
-from merged_data
-$$ language sql stable;
+CREATE
+OR
+replace FUNCTION ecosystem.active_ecdsa_accounts( period text, start_timestamp bigint DEFAULT 0, end_timestamp bigint DEFAULT CURRENT_TIMESTAMP::timestamp9::bigint )
+returns setof ecosystem.metric_total AS $$ WITH ecdsa_transactions AS
+(
+       SELECT t.consensus_timestamp,
+              t.payer_account_id
+       FROM   TRANSACTION t
+       JOIN   entity e
+       ON     t.payer_account_id = e.id
+       AND    t.consensus_timestamp BETWEEN start_timestamp AND    end_timestamp
+       AND    e.type = 'ACCOUNT'
+       AND    e.KEY IS NOT NULL
+       AND    e.created_timestamp IS NOT NULL
+       AND    (
+                     e.public_key LIKE '02%'
+              OR     e.public_key LIKE '03%' ) ), ecdsa_accounts_per_period AS
+(
+         SELECT   date_trunc(period, consensus_timestamp::timestamp9::timestamp) AS period_start_timestamp,
+                  count(DISTINCT payer_account_id)                               AS total
+         FROM     ecdsa_transactions
+         GROUP BY 1
+         ORDER BY 1 DESC )SELECT   int8range( period_start_timestamp::timestamp9::bigint, (lead(period_start_timestamp) OVER (ORDER BY period_start_timestamp rows BETWEEN CURRENT row AND      1 following))::timestamp9::bigint ),
+         total
+FROM     ecdsa_accounts_per_period $$ language sql stable;
