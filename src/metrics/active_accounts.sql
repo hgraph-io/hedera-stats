24fae608a5ee556080217b579323aecd089aef5d
itsbrandond <brandon@hgraph.com>
2025-06-25 18:11:44 -0400

    update active accounts function

--- src/metrics/active_accounts.sql
+++ src/metrics/active_accounts.sql
@@ -1,29 +1,41 @@
-create or replace function ecosystem.active_accounts(
-    period text,
-    start_timestamp bigint default 0,
-    end_timestamp bigint default CURRENT_TIMESTAMP::timestamp9::bigint
+CREATE OR REPLACE FUNCTION ecosystem.active_accounts(
+    period TEXT,
+    start_timestamp BIGINT DEFAULT 0,
+    end_timestamp   BIGINT DEFAULT CURRENT_TIMESTAMP::timestamp9::BIGINT
 )
-returns setof ecosystem . metric_total
-as $$
-with active_accounts as (
-  select *
-    from ecosystem.active_developer_accounts(period, start_timestamp, end_timestamp)
-  union all select *
-    from ecosystem.active_retail_accounts(period, start_timestamp, end_timestamp)
-  union all select *
-    from ecosystem.active_smart_contracts(period, start_timestamp, end_timestamp)
+RETURNS SETOF ecosystem.metric_total
+AS $$
+WITH active_accounts AS (
+    SELECT * FROM ecosystem.active_developer_accounts(period, start_timestamp, end_timestamp)
+    UNION ALL
+    SELECT * FROM ecosystem.active_retail_accounts  (period, start_timestamp, end_timestamp)
+    UNION ALL
+    SELECT * FROM ecosystem.active_smart_contracts  (period, start_timestamp, end_timestamp)
 ),
-merged_data as (
-    select lower(int8range) as period_start_timestamp, sum(total) as total
-  from active_accounts d
-  group by 1
-  order by 1 desc
+merged_data AS (
+    SELECT
+        DATE_TRUNC(
+            period,
+            (LOWER(int8range))::timestamp9::timestamp
+        )                         AS period_start,
+        SUM(total)                AS total
+    FROM   active_accounts
+    GROUP  BY 1
 )
-select
-    int8range(
-        period_start_timestamp::timestamp9::bigint,
-        coalesce((lead(period_start_timestamp) over (order by period_start_timestamp rows between current row and 1 following))::timestamp9::bigint, end_timestamp)
+SELECT
+    INT8RANGE(
+        period_start::timestamp9::BIGINT,
+        (CASE period
+            WHEN 'hour'    THEN period_start + INTERVAL '1 hour'
+            WHEN 'day'     THEN period_start + INTERVAL '1 day'
+            WHEN 'week'    THEN period_start + INTERVAL '1 week'
+            WHEN 'month'   THEN period_start + INTERVAL '1 month'
+            WHEN 'quarter' THEN period_start + INTERVAL '3 months'
+            WHEN 'year'    THEN period_start + INTERVAL '1 year'
+            ELSE period_start + INTERVAL '1 day'
+         END)::timestamp9::BIGINT
     ),
     total
-from merged_data
-$$ language sql stable;
+FROM   merged_data
+ORDER  BY period_start DESC;
+$$ LANGUAGE sql STABLE;
