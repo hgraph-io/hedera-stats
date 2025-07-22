00c697af19c9edd759fa3dc2267dc6fd9fb4c0ea
itsbrandond <brandon@hgraph.com>
2025-07-22 18:04:26 -0400

    new metrics

---
+++ src/metrics/total_transactions.sql
@@ -0,0 +1,38 @@
+CREATE OR REPLACE FUNCTION ecosystem.total_transactions (
+  period             public.interval_granularity,
+  start_timestamp    BIGINT DEFAULT  end_timestamp      BIGINT DEFAULT
+    (extract(epoch FROM current_timestamp) * 1e9)::BIGINT
+)
+RETURNS TABLE (timestamp_range INT8RANGE, total BIGINT)
+LANGUAGE SQL STABLE
+AS $$
+WITH base_total AS (
+  SELECT COUNT(*)::BIGINT AS base
+  FROM transaction
+  WHERE consensus_timestamp < start_timestamp
+),
+all_entries AS (
+  SELECT consensus_timestamp
+  FROM transaction
+  WHERE consensus_timestamp BETWEEN start_timestamp AND end_timestamp
+),
+periodized AS (
+  SELECT
+    date_trunc(period::TEXT,
+               to_timestamp(consensus_timestamp / 1e9)) AS period_start,
+    COUNT(*) AS total_transactions
+  FROM all_entries
+  GROUP BY 1
+  ORDER BY 1
+)
+SELECT
+  int8range(
+    (extract(epoch FROM period_start) * 1e9)::BIGINT,
+    COALESCE(
+      (extract(epoch FROM LEAD(period_start) OVER (ORDER BY period_start)) * 1e9)::BIGINT,
+      end_timestamp + 1 
+    )
+  ) AS timestamp_range,
+  base + SUM(total_transactions) OVER (ORDER BY period_start) AS total
+FROM periodized CROSS JOIN base_total;
+$$;
