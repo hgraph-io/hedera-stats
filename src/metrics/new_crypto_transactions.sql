6b922d614047235f527cb9d094ca3b888e92d6d6
itsbrandond <brandon@hgraph.com>
2025-08-05 15:48:57 -0400

    fixes for init procedures

--- src/metrics/new_crypto_transactions.sql
+++ src/metrics/new_crypto_transactions.sql
@@ -1,11 +1,10 @@
 -- New crypto transactions
 CREATE OR REPLACE FUNCTION ecosystem.new_crypto_transactions (
-  period             public.interval_granularity,
-  start_timestamp    BIGINT DEFAULT 0,
-  end_timestamp      BIGINT DEFAULT
-      (extract(epoch FROM current_timestamp) * 1e9)::BIGINT
+  period TEXT,
+  start_timestamp BIGINT DEFAULT 0,
+  end_timestamp BIGINT DEFAULT (extract(epoch FROM current_timestamp) * 1e9)::BIGINT
 )
-RETURNS TABLE (timestamp_range INT8RANGE, total BIGINT)
+RETURNS TABLE (int8range INT8RANGE, total BIGINT)
 LANGUAGE SQL STABLE
 AS $$
 WITH all_entries AS (
@@ -15,7 +14,7 @@ WITH all_entries AS (
     AND  type IN (10,11,12,13,14,15,48,49)
 ),
 periodized AS (
-  SELECT date_trunc(period::TEXT,
+  SELECT date_trunc(period,
                     to_timestamp(consensus_timestamp / 1e9)) AS period_start,
          COUNT(*) AS total
   FROM   all_entries
@@ -28,7 +27,7 @@ SELECT int8range(
            (extract(epoch FROM LEAD(period_start) OVER (ORDER BY period_start)) * 1e9)::BIGINT,
            end_timestamp + 1
          )
-       ) AS timestamp_range,
+       ) AS int8range,
        total
 FROM periodized;
 $$;
--- src/metrics/total_crypto_transactions.sql
+++ src/metrics/total_crypto_transactions.sql
@@ -1,5 +1,5 @@
--- Total other transactions
-CREATE OR REPLACE FUNCTION ecosystem.total_other_transactions (
+-- Total Crypto transactions
+CREATE OR REPLACE FUNCTION ecosystem.total_crypto_transactions (
   period TEXT,
   start_timestamp BIGINT DEFAULT 0,
   end_timestamp BIGINT DEFAULT (extract(epoch FROM current_timestamp) * 1e9)::BIGINT
@@ -11,7 +11,7 @@ WITH all_entries AS (
   SELECT consensus_timestamp
   FROM public.transaction
   WHERE consensus_timestamp <= end_timestamp
-    AND  type IN (20,21,23,28,42,43,44,51,52,54,55,56,65)
+    AND  type IN (10,11,12,13,14,15,48,49)
 ),
 periodized AS (
   SELECT date_trunc(period, to_timestamp(consensus_timestamp / 1e9)) AS period_start,
